4d9daad3b10ae0e34ece4042a4bd88e2eb21c681
mnguye95 <mnguye95@gmail.com>
2022-09-19 22:19:36 -0700

    projects updates v2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,12 +8,19 @@ module.exports = {
 				'honey-yellow' : '#fdb833',
 				'pine-green' : '#037971',
 				'light-cyan' : '#cdedf6',
-				'honeydew' : '#f0fff1'
+				'honeydew' : '#f0fff1',
+				'deep-saffron' : '#ff9b42',
+				'magic-mint' : '#acfcd9',
+				'cinnamon-satin' : '#d55672',
+				'blue-crayola' : '#5c7aff',
+				'raisin-black' : '#332e3c'
 			}
 		},
 		fontFamily: {
 			'space-grotesk': ['"Space Grotesk"', 'sans-serif'],
 			'montserrat': ['"Montserrat"', 'sans-serif'],
+			'bungee': ['"Bungee"', 'sans-serif'],
+			'germania': ['"Germania One"', 'cursive']
 		},
 		boxShadow: {
 			'rb' : '3px 3px 0 #fff'
