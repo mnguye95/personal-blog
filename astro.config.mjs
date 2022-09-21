0a6bcbd8f58ee722163b888d0666b746ee19322b
mnguye95 <mnguye95@gmail.com>
2022-09-21 13:36:14 -0700

    projects section, added contact form and functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
-
+import image from '@astrojs/image';
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), sitemap()]
+  integrations: [tailwind(), react(), sitemap(),image()]
 });
