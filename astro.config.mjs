ce38c30336ddd4794f5c7a7b1838d593011d47d8
mnguye95 <mnguye95@gmail.com>
2022-09-27 11:42:59 -0700

    config changes v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import react from '@astrojs/react';
 import image from '@astrojs/image';
 import sitemap from "@astrojs/sitemap";
 
-import vercel from "@astrojs/vercel/static";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), sitemap(), image()],
-  output: "server",
-  adapter: vercel()
+  // output: "server",
+  // adapter: vercel()
 });
