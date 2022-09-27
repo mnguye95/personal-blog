95d350c63d58d6f392350067001a35849c091b4a
mnguye95 <mnguye95@gmail.com>
2022-09-27 00:32:55 -0700

    changing astro config to fix ssr issue on vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 import image from '@astrojs/image';
 import sitemap from "@astrojs/sitemap";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
